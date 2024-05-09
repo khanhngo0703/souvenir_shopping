da99cf7e07bab988f03782a86419595f458e436e
khanhngo0703 <khanhcr2807@gmail.com>
2024-05-09 22:53:35 +0700

    first commit

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePageView from './views/HomePageView.vue';
+import DashboardView from './views/admin/DashboardView.vue';
+
+
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'HomePageView',
+        component: HomePageView,
+    },
+    {
+        path: '/das',
+        name: 'DashboardView',
+        component: DashboardView,
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+export default router
